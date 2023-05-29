0573daa0a08b5934fcac45b6c2ef8a8dde6413cc
tsutrof <jaysonfortus0815@gmail.com>
2023-05-29 13:29:00 +0800

    complete

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,8 +1,19 @@
 const toggleMenu = document.querySelector(".toggle__menu");
 const mobileNav = document.querySelector(".nav1");
 const header = document.querySelector(".header");
+const blurr = document.querySelector(".blur");
 toggleMenu.addEventListener("click", () => {
     toggleMenu.classList.toggle("open");
 	mobileNav.classList.toggle("open");
     header.classList.toggle("open");
+    blurr.classList.toggle("open");
+});
+
+
+const links = document.querySelectorAll(".drop-item h4");
+links.forEach((baba) => {
+	baba.addEventListener("click", () => {
+		baba.nextElementSibling.classList.toggle("open");
+		baba.querySelector("i").classList.toggle("open");
+	});
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,16 +13,22 @@ module.exports = {
         light:'#ffffff',
         dark:'#23272A',
         grey:'#f6f6f6',
+        blur:'rgba(0,0,0,.3);',
       },
 
       backgroundImage:{
         'mobile': "url('./img/mobile-banner.svg')",
-        "desktop": "url('./img/banner-bg1.svg')"
+        'desktop': "url('./img/banner-bg1.svg')",
+        'sparkle': "url('./img/sparkle.svg')",
       },
 
       boxShadow:{
         'md': '0 8px 15px rgba(0,0,0,.2);',
       },
+
+      screen: {
+        'lg': '1080px',
+      }
     },
   },
   plugins: [],
