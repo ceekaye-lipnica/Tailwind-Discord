2484dd21d18d8047f8125013d1052b7a625ef0a6
tsutrof <jaysonfortus0815@gmail.com>
2023-06-02 15:42:35 +0800

    updated

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,13 @@ const toggleMenu = document.querySelector(".toggle__menu");
 const mobileNav = document.querySelector(".nav1");
 const header = document.querySelector(".header");
 const blurr = document.querySelector(".blur");
+const body = document.querySelector("body");
 toggleMenu.addEventListener("click", () => {
     toggleMenu.classList.toggle("open");
 	mobileNav.classList.toggle("open");
     header.classList.toggle("open");
     blurr.classList.toggle("open");
+	body.style="overflow:hidden;"
 });
 
 
